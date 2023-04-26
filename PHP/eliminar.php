840737a3141890335e432b547bfc4ca2b4df9a11
Angel-sg-s <114315557+Angel-sg-s@users.noreply.github.com>
2023-04-25 23:55:36 -0500

    correcto

--- PHP/eliminar.php
+++ PHP/eliminar.php
@@ -11,14 +11,4 @@ $stmt->bindParam(':id', $id);
 $stmt->execute();
 $response = array('eliminado'=>TRUE);
 echo json_encode($response);
-
-// if ($stmt->rowCount() > 0) {
-//     // Si la eliminación fue exitosa, devolver una respuesta JSON con el indicador "eliminado" igual a true
-//     $response = array('eliminado' => true);
-//     
-// } else {
-//     // Si la eliminación falló, devolver una respuesta JSON con el indicador "eliminado" igual a false
-//     $response = array('eliminado' => false);
-//     echo json_encode($response);
-// }
 ?>
