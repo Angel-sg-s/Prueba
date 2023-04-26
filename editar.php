79892c3d8235123d2586352af08dbb24c3980c85
Angel-sg-s <114315557+Angel-sg-s@users.noreply.github.com>
2023-04-26 00:01:51 -0500

    c

---
+++ editar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Editar datos</title>
+</head>
+<body>
+	<form method="post" action="PHP/Editar.php">
+		<label for="nombre">Usuario:</label>
+		<input type="text" id="nombre" name="nombre"><br>
+
+		<label for="apellido">contraseña:</label>
+		<input type="text" id="apellido" name="apellido"><br>
+
+		<label for="email">Email:</label>
+		<input type="text" id="email" name="email"><br>
+
+		<input type="submit" value="Agregar">
+	</form>
+</body>
+</html>
